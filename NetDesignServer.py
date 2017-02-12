0fb035014d78669f367a260751c41272f0510dc3
EricsLaptop\Eric <eriico39hi@gmail.com>
2017-02-12 14:40:15 -0500

    Got transfer working. In current state destination image is unreadable until server close. (I assume file not being closed properly)

--- NetDesignServer.py
+++ NetDesignServer.py
@@ -9,7 +9,16 @@ from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('',serverPort))
+
+dstFile = 'dstPic.png'
+fileWrite = open(dstFile, 'ab')
+
+
 print ('The server is ready to receive')
 while 1:
     message, clientAddress = serverSocket.recvfrom(2048)
-    serverSocket.sendto(message, clientAddress)
+    fileWrite.write(message)
+    fileWrite.seek(2048)
+    if message == b"":
+        fileWrite.close()
+
