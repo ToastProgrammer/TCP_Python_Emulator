d7e9bb19148d2b4e4693ffe7b70a680760dd568d
EricDesktop <eriico39hi@gmail.com>
2017-03-05 16:10:04 -0500

    attempt to merge

--- DataFunctions.py
+++ DataFunctions.py
@@ -1,6 +1,5 @@
 from socket import *
 from Constants import *
-from binascii import *
 
 ##---------------Bitwise Manipulation-----------------##
 #------------------------------------------------------#
@@ -10,8 +9,6 @@ def ChecksumAddition(curSum, nextItem):
     if tempInt >= 65536: #overflow
         tempInt = (tempInt % 65536) + 1
     return(bytearray(tempInt.to_bytes(2, 'big')))
-
-#def BitsTo
 
 def MakeChecksum(bString):
     checksum = bytearray(b'\x00\x00')  ## Returns 16 bit bytes object (b'\x00\x00')
@@ -28,52 +25,34 @@ def MakeChecksum(bString):
     return checksum
 
 def CheckChecksum(bString):
-    checkString = MakeChecksum(bString[0:-2])
+    checkString = MakeChecksum(bString[2:])
     checkArray = bytearray(checkString)
-    recievedArray = bytearray(bString[0:-2])
+    recievedArray = bytearray(bString[0:2])
     if (checkArray[1] == recievedArray[1]) and (checkArray[0] == recievedArray[0]):
         return True
     else:
         return False
 
-def makepkt(data, seqnum, checksum):
+def InsertChecksum(data, checksum):
+    return bytes(checksum + data)
 
-    packet = data
-    packet = packet.append(seqnum)
-    packet = packet.append(checksum)
+def RemoveChecksum(bString):
+    return bString[2:]
 
-    return packet
+def AddSequenceNum(seq, seqNum):
+    return bytes([seqNum]) + seq
 
+def RemoveSequenceNum(seq):
+    return seq[1:]
 
+def CheckSequenceNum(seq, seqNum):
+    InSequence = False
+    if seq[1] is seqNum:
+        InSequence = True
+        return InSequence, RemoveSequenceNum(seq)
+    else:
+        return InSequence
 
-##-----------------Socket Functions-------------------##
-#------------------------------------------------------#
-def TransmitFile(fileRead):
-    # Setup socket
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-
-    # initial read of file
-    message = fileRead.read(PacketSize)
-    # loop to read and send packets to the server
-    while message != b"":
-        packet = message
-        clientSocket.sendto(packet, (ServerName, ServerPort))
-        message = fileRead.read(PacketSize)
-
-    # Send a final message to the server to signify end
-    clientSocket.sendto(TerminateCharacter, (ServerName, ServerPort))
-
-    # End by closing the file and the socket
-    clientSocket.close()
-
-def udt_send(sndpkt):
-    clientSocket.sendto(sndpkt, (ServerName, ServerPort))
-
-def rdt_rcv(rcvpkt):
-    rcvpkt, clientAddress = serverSocket.recvfrom(PacketSize)
-
-#bytearray(b'\x12\x5F\x3A')
-testing = open("srcPic.png", 'rb')
-t2 = testing.read(1024)
-print(type(t2))
-a = MakeChecksum(t2)
+def PackageHeader(data, seq):
+    Segment = AddSequenceNum(data, seq)
+    return InsertChecksum(Segment, MakeChecksum(Segment))
