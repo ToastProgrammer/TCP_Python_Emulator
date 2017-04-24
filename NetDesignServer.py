672efeef983e20f172a818d944f5bdf54e8418de
Ben-PC\Ben <benjitenji@gmail.com>
2017-04-23 23:18:22 -0400

    File now properly deletes upon starting a new file. No more constant appending

--- NetDesignServer.py
+++ NetDesignServer.py
@@ -11,26 +11,41 @@ from Constants import *
 from time import clock, sleep
 from os import remove
 
-global writeIndex
+writeIndex = 0
+moreData = True
+fileWrite = None
+newFile = True
+
 
 #---------------------------- D e l i v e r   D a t a --------------------------
-def deliver_data(data, writeIndex):
-    moreData = True
-    fileWrite = open(dstFile, 'ab')
-    fileWrite.seek(PacketSize * writeIndex, 0)
+def deliver_data(data):
+    global writeIndex
+    global moreData
+    global fileWrite
+    global newFile
+
+    if newFile:
+        fileWrite = open(dstFile, 'wb')
+        newFile = False
+    else:
+        fileWrite = open(dstFile, 'ab')
+    #fileWrite.seek(0)
+    #fileWrite.seek(PacketSize * writeIndex, 0)
     fileWrite.write(data)
     #if EOF close file
     writeIndex += PacketSize
     if data == b'':
         fileWrite.close()
         moreData = False
-    return moreData, writeIndex
 
 #---------------------------- S e r v e r   M a i n ----------------------------
 def ServerMain():
 
-    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    global writeIndex
+    global moreData
+    global newFile
 
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
     serverSocket.bind(('',ServerPort))
 
     print ('The server is ready to receive')
@@ -40,21 +55,20 @@ def ServerMain():
         print("Outer Loop")
         moreData = True
         clientDone = False
-        #onceThrough = False
-        writeIndex = 0
+
         expectedSeqNum = 1
-        test = 1
+        newFile = True
+
         sndpkt = PackageHeader(ACK,expectedSeqNum)
         while(moreData):
             rcvpkt = rdt_rcv(serverSocket)
             #print("Test", test)
-            test += 1
             #print(CheckChecksum(rcvpkt), CheckSequenceNum(rcvpkt, expectedSeqNum))
             if CheckChecksum(rcvpkt) and CheckSequenceNum(rcvpkt, expectedSeqNum):  # If Checksum & seq num correct
                 expectedSeqNum += 1
                 data = UnpackageHeader(rcvpkt)
                 print(rcvpkt[0:5])
-                moreData, writeIndex = deliver_data(data, writeIndex)  # Write correct data to file
+                deliver_data(data)  # Write correct data to file
                 sndpkt = PackageHeader(ACK, expectedSeqNum)  # Package CORRECT ack
 
                 udt_send(sndpkt, serverSocket, ClientPort)
